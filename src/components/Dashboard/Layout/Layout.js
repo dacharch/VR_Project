56e1d5d98ab84ef880514051d853123f1d92e364
dachman <dachman@dachman.dachman>
2024-12-01 22:33:05 +0530

    Layout is done

--- src/components/Dashboard/Layout/Layout.js
+++ src/components/Dashboard/Layout/Layout.js
@@ -1,67 +1,171 @@
 import * as React from 'react';
 import Box from '@mui/material/Box';
 import Drawer from '@mui/material/Drawer';
-import CssBaseline from '@mui/material/CssBaseline';
-import AppBar from '@mui/material/AppBar';
-import Toolbar from '@mui/material/Toolbar';
 import List from '@mui/material/List';
-import Divider from '@mui/material/Divider';
 import ListItem from '@mui/material/ListItem';
 import ListItemButton from '@mui/material/ListItemButton';
 import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
-import InboxIcon from '@mui/icons-material/MoveToInbox';
-import MailIcon from '@mui/icons-material/Mail';
+import InputAdornment from '@mui/material/InputAdornment';
+import GroupIcon from '@mui/icons-material/Group';
+import DoneAllIcon from '@mui/icons-material/DoneAll';
+import RemoveDoneIcon from '@mui/icons-material/RemoveDone';
+import LogoutIcon from '@mui/icons-material/Logout';
+import { Button, TextField, } from '@mui/material';
+import SearchIcon from '@mui/icons-material/Search';
+import { useState } from 'react';
+import Users from "../Users/Users" ;
+import "./layout.css"
 
-const drawerWidth= "240" ;
+const drawerWidth = "240";
 
-const  NAVIGATION = [
-    
-] ;
+const NAVIGATION = [
+  {
+    number: 0,
+    title: "All Users",
+    icon: <GroupIcon />
+  },
+  {
+    number: 1,
+    title: "Active Users",
+    icon: <DoneAllIcon />
+
+  },
+  {
+    number: 2,
+    title: "Inactive Users",
+    icon: <RemoveDoneIcon />
+  },
+  {
+    number: 3,
+    title: "Logout",
+    icon: <LogoutIcon />
+  }
+
+];
 
 
 
 
 const Layout = () => {
-  return (
-    <Box sx={{ display: 'flex' }}>
-    <CssBaseline />
+  const [selectedIndex, setSelectedIndex] = useState(0);
+
+  const handleListItem = (number) => {
+     setSelectedIndex(number) ;
+  }
+
+  const BottomContent = ()=>{
+    
+    switch(selectedIndex){
+       case 0: return <Users/>
+             
+    }
+     
   
-    <Drawer
-      sx={{
-        width: drawerWidth,
-        flexShrink: 0,
-        '& .MuiDrawer-paper': {
-          width: drawerWidth,
-          marginTop:`63px`,
-          boxSizing: 'border-box',
-        },
-      }}
-      variant="permanent"
-      anchor="left"
-    >
-      
-      <List>
-        {['All Users', 'Active Users', 'Inactive Users'].map((text, index) => (
-          <ListItem key={text} disablePadding>
-            <ListItemButton>
-              <ListItemIcon>
-                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-              </ListItemIcon>
-              <ListItemText primary={text} />
-            </ListItemButton>
-          </ListItem>
-        ))}
-      </List>
-    </Drawer>
-    <Box
-      component="main"
-      sx={{ flexGrow: 1, bgcolor: 'background.default', p: 34}}
-    >
-      <Toolbar />
-   
-    </Box>
-  </Box>
+  }
+  return (
+    <div className='layout_container'>
+
+      <div>
+        <Drawer
+          sx={{
+            width: drawerWidth,
+            flexShrink: 0,
+            '& .MuiDrawer-paper': {
+              width: drawerWidth,
+              marginTop: `4rem`,
+              boxSizing: 'border-box',
+            },
+          }}
+          variant="permanent"
+          anchor="left"
+        >
+
+          <List>
+            {NAVIGATION.map((item) => (
+              <ListItem key={item.number} disablePadding>
+                <ListItemButton
+                  selected={selectedIndex === item.number}
+                  onClick={() => handleListItem(item.number)}
+                  sx={{
+                    '&.Mui-selected': {
+                      backgroundColor: '#6200ea',
+                      color: 'red',
+                    },
+                    '&.Mui-selected:hover': {
+                      backgroundColor: '#4500b5',
+                    },
+                  }}
+                >
+                  <ListItemIcon>
+                    {item.icon}
+                  </ListItemIcon>
+                  <ListItemText primary={item.title} />
+                </ListItemButton>
+              </ListItem>
+            ))}
+          </List>
+        </Drawer>
+        <Box
+          component="main"
+          sx={{ bgcolor: 'background.default', p: 12 }}
+        >
+        </Box>
+
+      </div>
+      <div className='right_container'>
+        <Box className="search_container" component="section">
+          <Box>
+            <TextField
+              variant="outlined"
+              placeholder="Search User..."
+
+              sx={{
+                width: '100%',
+                maxWidth: 300,
+                backgroundColor: '#f5f5f5',
+                borderRadius: 2,
+                '& .MuiOutlinedInput-root': {
+                  borderRadius: 2,
+                  '& fieldset': {
+                    borderColor: '#ccc',
+                  },
+                  '&:hover fieldset': {
+                    borderColor: '#888',
+                  },
+                  '&.Mui-focused fieldset': {
+                    borderColor: '#6200ea',
+                    boxShadow: '0 0 8px rgba(98, 0, 234, 0.4)',
+                  },
+                },
+              }}
+
+              InputProps={{
+                startAdornment: (
+                  <InputAdornment position="start">
+                    <SearchIcon sx={{ color: '#6200ea' }} />
+                  </InputAdornment>
+                )
+              }}
+
+            />
+
+          </Box>
+          <Box>
+            <Button
+              size='medium'
+              variant='contained'
+            >
+              Add New User
+            </Button>
+          </Box>
+        </Box>
+
+        <Box>
+            <BottomContent/>
+        </Box>
+      </div>
+    </div>
   )
 }
 
